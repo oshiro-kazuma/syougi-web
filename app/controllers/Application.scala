0fec88b323e9c90ed62131fff85f99e8a9bebaaf
oshiro-kazuma <ariteku@gmail.com>
2016-02-14 00:22:53 +0900

    integration syougi

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,15 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def battle = Action {
+    Ok(views.html.battle())
+  }
+
+  def history = Action {
+    Ok(views.html.history())
   }
 
 }
