2029f19b28f2c1a7dd31c68f1e2ab90e99def331
oshiro-kazuma <ariteku@gmail.com>
2016-02-14 10:56:54 +0900

    move Application to ApplicationController

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+class ApplicationController extends Controller {
 
   def index = Action {
     Ok(views.html.index())
